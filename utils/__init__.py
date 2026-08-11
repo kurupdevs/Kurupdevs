36967d235d27abdd5d0335832cad7eed9c88a90d
Kurup <160393536+kurupdevs@users.noreply.github.com>
2026-08-11 19:15:56 +0530

    Delete utils directory

--- utils/__init__.py
+++
@@ -1,6 +0,0 @@
-"""KurupDevs utilities package."""
-from .config import getc, setc
-from .db import get, setv, remove
-from .scripts import progress, safe_edit, safe_del, parse_args
-
-__all__=["getc","setc","get","setv","remove","progress","safe_edit","safe_del","parse_args"]
