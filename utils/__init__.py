de2afa1e00efe0c1362d4c273f29be14cf529cab
Kurup <160393536+kurupdevs@users.noreply.github.com>
2026-08-11 18:11:20 +0530

    chore: prune unused imports

--- utils/__init__.py
+++ utils/__init__.py
@@ -4,4 +4,4 @@ from sys import version_info
 python_version = f"{version_info[0]}.{version_info[1]}.{version_info[2]}"
 modules_help = {}
 requirements_list = []
-prefix = "."
+prefix = "."  # default command prefix
